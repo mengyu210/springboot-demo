5dd88072c4863b3a41468b532dbd7df8a1007a48
mengyu210 <2352441187@qq.com>
2020-08-27 22:48:02 +0800

    util 项目

---
+++ springboot-util/src/test/java/com/gyp/springboot/SpringbootUtilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gyp.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootUtilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
